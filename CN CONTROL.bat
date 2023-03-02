044f85b127c268991651e8e4a0b6a88c87cb95f7
BenjaLuke <benjaminluckyluke@gmail.com>
2023-03-02 08:12:20 +0100

    Aviso de incidencias

--- CN CONTROL.bat
+++ CN CONTROL.bat
@@ -2,7 +2,7 @@ echo off
 echo CN CONTROL V.3.15.16
 echo by: Benjamin Miguel Miralpeix
 echo 2021/2023
-echo ultima actualizacion: 2023/03/01
+echo ultima actualizacion: 2023/03/02
 echo -
 echo -
 echo ------------------------------------------------------------
