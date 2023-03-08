39c32935dfc18284000c88a5fe996662398e35de
BenjaLuke <benjaminluckyluke@gmail.com>
2023-03-08 06:30:01 +0100

    Lista button everywhere

--- CN CONTROL.bat
+++ CN CONTROL.bat
@@ -1,8 +1,8 @@
 echo off
-echo CN CONTROL V.3.15.29
+echo CN CONTROL V.3.15.32
 echo by: Benjamin Miguel Miralpeix
 echo 2021/2023
-echo ultima actualizacion: 2023/03/07
+echo ultima actualizacion: 2023/03/08
 echo -
 echo -
 echo ------------------------------------------------------------
